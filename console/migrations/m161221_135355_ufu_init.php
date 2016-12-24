d573e9e2c0c168f4f64b1cef5b82b0adb27760bf
Vitalii Zagorodniuk <vitaliizagorodniuk@gmail.com>
2016-12-24 14:40:20 +0200

    Update user friendly extension

---
+++ console/migrations/m161221_135355_ufu_init.php
@@ -0,0 +1,10 @@
+<?php
+require(Yii::getAlias('@vendor/xz1mefx/yii2-ufu/migrations/m161223_113345_ufu_init.php'));
+
+/**
+ * Class m161221_135355_hfu_init
+ */
+class m161221_135355_ufu_init extends m161223_113345_ufu_init
+{
+
+}
