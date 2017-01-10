4525cae518ec7729b0b006cff2c9c47b2cbbf609
Vitalii Zagorodniuk <vitaliizagorodniuk@gmail.com>
2017-01-10 17:07:39 +0200

    Update product model

--- common/models/Product.php
+++ common/models/Product.php
@@ -273,6 +273,7 @@ class Product extends UfuActiveRecord
             'description' => Yii::t('common', 'Description'),
             'currency.code' => Yii::t('common', 'Currency'),
             'currency.name' => Yii::t('common', 'Currency'),
+            'mainImage' => Yii::t('common', 'Main image'),
         ];
     }
 
